080fca90d69674a5cf1d3dbdfd245ab5df1b153f
Mega Core 2 <107163595+megacorep2@users.noreply.github.com>
2022-06-09 02:57:14 -0400

    Update pre-kitchen.rb

--- pre-kitchen.rb
+++ pre-kitchen.rb
@@ -2,6 +2,22 @@
 require 'yaml'
 
 @run_kitchen = true
+
+my_vars.each do |value|
+  if ENV.fetch(value,nil).to_s != ''
+    puts "#{value} is: " + ENV.fetch(value,nil).to_s
+  else
+    @run_kitchen=false
+    puts "Please set #{value}"
+  end
+end
+
+puts '----------'
+puts 'You are OK to run Test Kitchen' if @run_kitchen == true
+puts 'Please SET the above Envrioment variables before running kitchen' if @run_kitchen == false
+
+require 'rubygems'; require 'json';
+
 my_vars =
   [
     'AWS_ACCESS_KEY_ID',
@@ -13,24 +29,6 @@ my_vars =
     'AWS_DEFAULT_INSTANCE_TYPE',
     'SSH_SG_CIDR',
   ]
-
-my_vars.each do |value|
-  if ENV.fetch(value,nil).to_s != ''
-    puts "#{value} is: " + ENV.fetch(value,nil).to_s
-  else
-    @run_kitchen=false
-    puts "Please set #{value}"
-  end
-end
-
-puts "If you want to get JSON output, please set the USE_JSON = 'true'"
-puts "TF_VAR_aws_ssh_key_id' = test1212s"
-puts ''
-puts '----------'
-puts 'You are OK to run Test Kitchen' if @run_kitchen == true
-puts 'Please SET the above Envrioment variables before running kitchen' if @run_kitchen == false
-
-require 'rubygems'; require 'json';
 
 
 my_vpcs =  JSON.parse(%x[aws ec2 describe-vpcs]);
